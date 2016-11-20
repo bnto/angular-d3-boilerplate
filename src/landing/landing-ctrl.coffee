14755cec169a39133c7076e97c85d90d99af7a64
bnto <bnto@users.noreply.github.com>
2016-11-20 11:47:54 +0100

    Completed technical test :raised_hands:

--- src/landing/landing-ctrl.coffee
+++ src/landing/landing-ctrl.coffee
@@ -1,19 +1,19 @@
-angular.module '%module%.landing'
-.controller 'LandingCtrl',
-($scope, $http) ->
-  $scope.someText = "Test"
+# angular.module '%module%.landing'
+# .controller 'LandingCtrl',
+# ($scope, $http) ->
+#   $scope.someText = "Test"
 
-  $scope.users = []
+#   $scope.users = []
 
-  $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN']
+#   $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN']
 
-  $http.get 'data/users.csv'
-  .then (res) ->
-    $scope.users = Papa.parse(
-      res.data
-    ,
-      header: true
-      delimiter: ','
-      dynamicTyping: true
-      skipEmptyLines: true
-    ).data
+#   $http.get 'data/users.csv'
+#   .then (res) ->
+#     $scope.users = Papa.parse(
+#       res.data
+#     ,
+#       header: true
+#       delimiter: ','
+#       dynamicTyping: true
+#       skipEmptyLines: true
+#     ).data
--- src/landing/landing-ctrl.js
+++ src/landing/landing-ctrl.js
@@ -1,18 +1,42 @@
-// To use this, comment the .coffee version and uncomment this one
+angular.module('%module%.landing')
+.controller('LandingCtrl', function($scope, $http) {
 
-// angular.module('%module%.landing')
-// .controller('LandingCtrl', function($scope, $http) {
-//
-//   $scope.users = [];
-//   $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN'];
-//
-//   $http.get('data/users.csv')
-//   .then(function(res) {
-//     $scope.users = Papa.parse(res.data, {
-//       header: true,
-//       delimiter: ',',
-//       dynamicTyping: true,
-//       skipEmptyLines: true
-//     }).data;
-//   });
-// });
+  $scope.users = [];
+  $scope.availableRoles = ['USER', 'EDITOR', 'ADMIN'];
+  $scope.expand = false;
+
+  $http.get('data/users.csv')
+  .then(function(res) {
+    $scope.users = Papa.parse(res.data, {
+      header: true,
+      delimiter: ',',
+      dynamicTyping: true,
+      skipEmptyLines: true
+    }).data;
+  });
+  
+
+  $scope.getUserInitials = function(firstname, lastname) {
+  	// first (& second if applicable) initials of firstname + last initial of lastname
+  	return firstname.match(/\b([a-zA-Z])/g).slice(0,2).join('') + lastname.match(/(\w)\w*$/)[1]
+  };
+
+  $scope.parseUserRoles = function(roles) {
+  	return roles.split(';').join(', ');
+  };
+  $scope.checkRoles = function (item, list) {
+  	return list.indexOf(item) > -1;
+  };
+  $scope.updateRoles = function (item, list) {
+  	list = (list === '') ? [] : list.split(';');
+  	var idx = list.indexOf(item);
+  	if (idx > -1) {
+  		list.splice(idx, 1);
+  	}
+  	else {
+  		list.push(item);
+  	};
+  	return (list === []) ? '' : list.join(';');
+  };
+
+});
